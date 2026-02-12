eb10d3c7924fd2fb8a5f312e6eca24dbf4f2afb2
Issac Liu <issac@matchatea.dev>
2026-02-12 14:29:43 -0500

    chore: move RGBIndicator to subsystems

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/RGBIndicator.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/RGBIndicator.java
@@ -1,5 +1,7 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.subsystems;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
@@ -88,5 +90,28 @@ public class RGBIndicator {
      */
     public Servo getInternalServo() {
         return servo;
+    }
+
+    @SuppressWarnings("unused")
+    @TeleOp(name = "Test RGB Indicator - Color Sweep", group = "testing")
+    public static class ColorSweepOpMode extends LinearOpMode {
+        @Override
+        public void runOpMode() throws InterruptedException {
+            waitForStart();
+
+            RGBIndicator led = RGBIndicator.get(hardwareMap, "led");
+
+            while (opModeIsActive()) {
+                // sweep through colors, from 0.279 to 0.723 then repeat
+                for (double pos = 0.279; pos <= 0.723; pos += 0.001) {
+                    led.setColor(pos);
+                    sleep(1);
+                }
+                for (double pos = 0.723; pos >= 0.279; pos -= 0.001) {
+                    led.setColor(pos);
+                    sleep(1);
+                }
+            }
+        }
     }
 }
