631d403d5477f59d59ffbc55067dd980c8d71323
Issac Liu <issac@matchatea.dev>
2026-02-24 12:24:47 -0500

    feat: add ControllerLights opmode for LED color control

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ControllerLights.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "Controller Lights", group = "Testing")
+public class ControllerLights extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        waitForStart();
+
+        while (opModeIsActive()) {
+            double mappedR = gamepad1.left_trigger;
+            double gamepad1Touch1Y = gamepad1.touchpad_finger_1_y;
+            double mappedB = gamepad1.right_trigger;
+
+            double mappedG = (gamepad1Touch1Y + 1) / 2; // Map from [-1, 1] to [0, 1]
+
+            if (gamepad1.aWasReleased()) {
+                gamepad1.setLedColor(mappedR, mappedG, mappedB, 10000);
+            }
+
+            telemetry.addData("Red", mappedR);
+            telemetry.addData("Green", mappedG);
+            telemetry.addData("Blue", mappedB);
+            telemetry.update();
+        }
+    }
+}
