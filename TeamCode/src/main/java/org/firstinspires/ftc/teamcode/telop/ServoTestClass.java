bfd1fb461a83aec0804eca1c26c97999540a56b8
Issac Liu <issac@matchatea.dev>
2026-02-26 18:05:17 -0500

    feat: add ServoTestClass for testing servo control in teleop mode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/telop/ServoTestClass.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode.telop;
+
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp(name = "Servo Test Class", group = "Testing")
+public class ServoTestClass extends LinearOpMode {
+    Servo serville;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        serville = hardwareMap.get(Servo.class, "serville");
+
+        waitForStart();
+        while (opModeIsActive()) {
+            if (gamepad1.dpadDownWasReleased()) {
+                serville.setPosition(0);
+            } else if (gamepad1.dpadUpWasReleased()) {
+                serville.setPosition(0.5);
+            }
+        }
+    }
+
+}
