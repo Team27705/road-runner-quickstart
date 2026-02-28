d7f3edca0d78c551f06efa95e71d5120bccac37e
Issac Liu <issac@matchatea.dev>
2026-02-28 12:03:37 -0500

    refactor: extract ColorSensor class for improved artifact detection and state management

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Spindexer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Spindexer.java
@@ -10,6 +10,7 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.teamcode.subsystems.hardwares.ColorSensor;
 import org.firstinspires.ftc.teamcode.subsystems.hardwares.RTPTorctex;
 
 import java.util.Arrays;
@@ -47,8 +48,6 @@ public class Spindexer {
     private int teleopMotif;
     private final ElapsedTime bootKickerTimer;
     private int currentTargetMotifNum = 0;
-
-    private int r, g, b, opacity;
 
 
     //https://gm0.org/en/latest/docs/software/concepts/finite-state-machines.html
@@ -373,93 +372,6 @@ public class Spindexer {
 
         }
         return false;
-    }
-
-    public class ColorSensor {
-        public static final int OPACITY_NO_OBJECT = 300000000;
-        private static final int TIME_TO_DETECT = 25; //millis TODO: Increase delay and test
-        public static final int RGB_MIN = 50;
-        public static final int RGB_MAX = 600;
-        private final RevColorSensorV3 revColorSensor;
-        private final Spindexer spin;
-
-        // State
-        private boolean colorFound;
-        private final ElapsedTime colorSensorTimer;
-        private double colorStartTime;
-        public boolean artifactDetected;
-
-
-        public ColorSensor(RevColorSensorV3 revColorSensor, Spindexer spindexer) {
-            this.revColorSensor = revColorSensor;
-            this.spin = spindexer;
-            colorSensorTimer = new ElapsedTime();
-        }
-
-        public void update(int currentChamber, String[] inventory) {
-//         NormalizedRGBA colors = colorSensor.getNormalizedColors();
-//         RGB = new double[] {colors.red, colors.green, colors.blue};
-
-            String colorDetected = detectArtifactColor();
-
-            if (colorDetected.equals("E")) {
-                colorFound = false;
-                artifactDetected = false;
-                return;
-            }
-
-            if (!colorFound) {
-                colorFound = true;
-                colorSensorTimer.reset();
-                colorStartTime = colorSensorTimer.milliseconds();
-            }
-
-            if (colorSensorTimer.milliseconds() - colorStartTime >= TIME_TO_DETECT) {
-                if (spin.inventory[spin.currentChamber].equals("E")) {
-                    spin.inventory[spin.currentChamber] = colorDetected;
-                    artifactDetected = true;
-                }
-                colorStartTime = 0;
-                colorFound = true;
-                //deactivate colorSensor
-            }
-            else {
-                artifactDetected = false;
-            }
-        }
-        //TODO: RETUNE COLOR SENSOR
-        //TODO: Optional get feedback from the GoBuilda light when updating/detecting a artifact
-        public String detectArtifactColor() {
-            r = revColorSensor.red(); //test if you dont need remove g> r and b > r and see if it still works
-            g = revColorSensor.green();
-            b = revColorSensor.blue();
-            opacity = getARGB();
-            if (opacity >= OPACITY_NO_OBJECT) { // higher opacity means no object detected
-                return "E";
-            } else if (g > r && g > b && g > RGB_MIN && g < RGB_MAX) { //sometimes g or b will go to some absurd value in the hundreds
-                return "G";
-            } else if (b > r && b > g && b > RGB_MIN && b < RGB_MAX) { // for purple check
-                return "P";
-            } else return "E";
-        }
-
-        public int getARGB() {
-            return revColorSensor.argb();
-        }
-
-        public boolean isArtifactDetected() {
-            return artifactDetected;
-        }
-
-        public String log() {
-
-            return "Red: " + r
-                    + "\nGreen: " + g
-                    + "\nBlue: " + b
-                    + "\nARGB: " + getARGB()
-                    + "\nArtifact Color: " + detectArtifactColor() +
-                    "Is able to spin: " + canSpin;
-        }
     }
 
     public void setMotif(int motifTagNum) { //may not be needed
